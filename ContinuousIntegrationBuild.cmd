6cc87843679a9e59a82bc17dcc92e01a8afb7254
rolfvdhulst <37153373+rolfvdhulst@users.noreply.github.com>
2020-07-30 15:14:31 +0200

    [CI] attempt to fix pathing

--- ContinuousIntegrationBuild.cmd
+++ ContinuousIntegrationBuild.cmd
@@ -1,5 +1,6 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build\scripts\build.ps1" -restore %*
-echo Start building compilers..
-msbuild "%~dp0\Compilers.sln" /v:m
+echo Start restoring Roslyn ...
+call "%~dp0\Roslyn\Restore.cmd"
+echo Start building Roslyn Compiler ...
+msbuild "%~dp0\Roslyn\Compilers.sln" /v:m
